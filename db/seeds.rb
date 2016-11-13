59791a87ae97931aad9c2c820ec88236a5e13f7e
SEONGHUN KANG <neuron03@gmail.com>
2016-11-13 20:22:44 +0900

    간단한 수정

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 관리자 계정의 생성 및 역할지정(관리자가 생성된 후 보안상의 이유로 비밀번호는 반드시 다른 것으로 변경하기 바람.)
-admin_user = User.create( email: 'admin@email.com', password: '12345678')
+admin_user = User.create(name: '관리자' email: 'admin@email.com', password: '12345678')
 admin_user.add_role :admin
